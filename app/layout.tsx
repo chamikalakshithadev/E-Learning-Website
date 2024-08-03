9ff47d6a3243b089b203ec1a39f4fbb97c6d8b2d
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-08-03 21:01:54 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/NavBar/ResponsiveNav";
 
 const font = Space_Grotesk({
   subsets: ["latin"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ResponsiveNav />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Home/NavBar/Nav.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { navLinks } from "@/constant/constant";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
+
+const Nav = () => {
+  const [navBg, setNavBg] = useState(false);
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) {
+        setNavBg(true);
+      }
+      if (window.scrollY < 90) {
+        setNavBg(false);
+      }
+    };
+    window.addEventListener("scroll", handler);
+
+    return () => window.removeEventListener("scroll", handler);
+  });
+  return (
+    <div
+      className={`fixed ${
+        navBg ? "bg-indigo-800" : "fixed"
+      }  w-full transition-all duration-200 h-[12vh] z-[1000] bg-blue-700`}
+    >
+      <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+        {/* logo */}
+        <Image src="/images/logo.png" alt="Logo" width={120} height={120} />
+        {/* nav links */}
+        <div className="hidden lg:flex items-center space-x-10">
+          {navLinks.map((link) => {
+            return (
+              <Link key={link.id} href={link.url}>
+                <p className="nav__link">{link.label}</p>
+              </Link>
+            );
+          })}
+        </div>
+        {/* buttons */}
+        <div className="flex items-center space-x-4">
+          <button className="md:px-10 md:py-2 px-8 py-1.5 text-white font-semibold text-base bg-pink-700 hover:bg-pink-900 transition-all duration-200 rounded-lg">
+            SignUp Now
+          </button>
+
+          {/* burger menu */}
+          <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-white lg:hidden " />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
